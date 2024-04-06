815d51328b5aba55d086cb6adf649d502ed9d1dd
arpit-agr <arpit.agrawal2502@gmail.com>
2024-04-06 13:46:28 +0530

    configure shiki theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
-import sitemap from "@astrojs/sitemap";
+import {defineConfig} from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		shikiConfig: {theme: 'vesper'}
+	},
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
-			nesting: true,
+			nesting: true
 		}),
 		partytown(),
-		sitemap(),
-	],
+		sitemap()
+	]
 });
