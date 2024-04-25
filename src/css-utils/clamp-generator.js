91b5b6f1ccf1f8da96630804c7d08a6c1659065e
arpit-agr <arpit.agrawal2502@gmail.com>
2024-04-25 09:30:28 +0530

    udpate css utils

--- src/css-utils/clamp-generator.js
+++ src/css-utils/clamp-generator.js
@@ -1,4 +1,4 @@
-const viewports = require('../design-tokens/viewports.json');
+import {min as _min, max as _max} from '../design-tokens/viewports.json';
 
 /**
  * Takes an array of tokens and sends back and array of name
@@ -20,20 +20,29 @@ const clampGenerator = tokens => {
 		const maxSize = max / rootSize;
 
 		// Convert the pixel viewport sizes into rems
-		const minViewport = viewports.min / rootSize;
-		const maxViewport = viewports.max / rootSize;
+		const minViewport = _min / rootSize;
+		const maxViewport = _max / rootSize;
 
 		// Slope and intersection allow us to have a fluid value but also keep that sensible
 		const slope = (maxSize - minSize) / (maxViewport - minViewport);
 		const intersection = -1 * minViewport * slope + minSize;
 
-		return {
-			name,
-			value: `clamp(${minSize}rem, ${intersection.toFixed(2)}rem + ${(
-				slope * 100
-			).toFixed(2)}vw, ${maxSize}rem)`
-		};
+		if (minSize > maxSize) {
+			return {
+				name,
+				value: `clamp(${maxSize.toFixed(2)}rem, ${intersection.toFixed(2)}rem + ${(
+					slope * 100
+				).toFixed(2)}vw, ${minSize.toFixed(2)}rem)`
+			};
+		} else {
+			return {
+				name,
+				value: `clamp(${minSize.toFixed(2)}rem, ${intersection.toFixed(2)}rem + ${(
+					slope * 100
+				).toFixed(2)}vw, ${maxSize.toFixed(2)}rem)`
+			};
+		}
 	});
 };
 
-module.exports = clampGenerator;
+export default clampGenerator;
