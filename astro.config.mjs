712a623f06b948b26630915f16dd7d5d948b6420
arpit-agr <arpit.agrawal2502@gmail.com>
2024-04-25 09:02:33 +0530

    set site option in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://arpit.codes',
 	markdown: {
 		shikiConfig: {theme: 'vesper'}
 	},
