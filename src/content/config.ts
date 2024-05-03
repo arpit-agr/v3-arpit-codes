a35908166db20d34606177f63a87016dda773290
arpit-agr <arpit.agrawal2502@gmail.com>
2024-05-03 08:48:18 +0530

    add max length constraint for blog post description

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const blogCollection = defineCollection({
 	type: 'content',
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
+		description: z.string().max(160),
 		pubDate: z.date(),
 		tags: z.array(z.string()),
 		image: z
