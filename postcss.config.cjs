cf1af0dd7ea05f98628cc5d8d4c664d3334b2874
arpit-agr <arpit.agrawal2502@gmail.com>
2024-05-29 15:22:14 +0530

    update postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,9 +3,9 @@ const OpenProps = require('open-props');
 
 module.exports = {
 	plugins: [
-		postcssJitProps(OpenProps),
-		require('tailwindcss/nesting'),
-		require('postcss-custom-media'),
-		require('tailwindcss')
+		postcssJitProps(OpenProps), // Load custom properties first
+		require('tailwindcss/nesting'), // Apply nesting
+		require('tailwindcss'), // Generate Tailwind utilities
+		require('postcss-custom-media') // Apply custom media queries
 	]
 };
